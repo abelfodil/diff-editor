9147aaa8cb67b2781d82a84cbce390b7885fbde8
Anes Belfodil <ans.belfodil@gmail.com>
2020-04-21 17:24:12 -0400

    Add language selector

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { MonacoDiffEditor } from "react-monaco-editor";
+import LanguageSelector from "./LanguageSelector";
 
-const Editor = () => (
-  <div>
-    <MonacoDiffEditor
-          width="1200"
-          height="800"
-          language="javascript"
-          options={{theme: "vs-dark", originalEditable: true}}
-        />
-  </div>
-);
+const Editor = () => {
+    return (
+        <div>
+            <LanguageSelector />
+            <MonacoDiffEditor
+                width="1200"
+                height="800"
+                language="javascript"
+                options={{ theme: "vs-dark", originalEditable: true }}
+            />
+        </div>
+    );
+};
 
 export default Editor;
